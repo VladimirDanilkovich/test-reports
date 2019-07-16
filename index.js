90d6b407c47765c3626f659df7b2efb6c0bf010a
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-16 16:58:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
-var assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(4), -1);
-    });
-  });
-});
+const {Builder, By, until} = require('selenium-webdriver');
+
+(async function example() {
+    const driver = await new Builder().forBrowser('chrome').build();
+
+    try {
+        await driver.get('https://www.google.com');
+        await driver.findElement(By.name('q')).sendKeys('nehalist');
+        await driver.findElement(By.id('tsf')).submit();
+        await driver.wait(until.elementLocated(By.id('search')));
+        await driver.findElement(By.linkText('nehalist.io')).click();
+        await driver.wait(until.titleIs('nehalist.io'));
+    } finally {
+        await driver.quit();
+    }
+})();
