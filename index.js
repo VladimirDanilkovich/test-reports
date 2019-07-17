f12c0d94b0be6f0c5cf88ea6ec3b660347f4c71e
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 13:11:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var driver = new webdriver.Builder()
 describe.only('#flattenArray()', function() {
    it('Test_1',  function() {
      driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
-     driver.setRect(1440, 802);  
+     driver.findElement(By.xpath("//div/input")).sendKeys("Selenium");
   });
 })
