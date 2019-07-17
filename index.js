c8f670bcd9f81d73b05b3c9ce32595754b516865
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 17:01:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,8 @@ describe('#promiseMd5()', function() {
   context('with string argument', function() {
     it('should compute MD5 hash', async function() {
     
-      // use await to wait until the promise is fulfilled
-      var hash = await promiseMd5('Glad Chinda');
-      
-      // add some assertions
-      expect(hash)
-        .to.be.a('string')
-        .that.matches(/^[a-f0-9]{32}$/)
-        .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
+      await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
     })
     
-  })
-  context('with non-string argument', function() {
-    it('should throw an error', async function() {
-    
-      await promiseMd5(12345).catch(function(err) {
-        // add an assertion to check the error
-        expect(function() { throw err })
-          .to.throw(TypeError, 'Data must be a string or a buffer');
-       })
-       
-    })
-  })
-  
+  })  
 })
