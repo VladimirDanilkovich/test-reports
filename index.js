7e2e32adbeaa1bb20388c32e934ef5cf4d5ba824
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 11:18:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-beforeEach(async function() {
-  await db.clear();
-  await db.save([tobi, loki, jane]);
-});
-
-describe('#find()', function() {
-  it('responds with matching records', async function() {
-    const users = await db.find({type: 'User'});
-    users.should.have.length(3);
-  });
-});
+describe('#flattenArray()', function() {
+  // a pending test
+  it('should flatten array');
+})
