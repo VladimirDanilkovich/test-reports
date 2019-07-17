ac0b6b014d115bb4e4d6b07289eceb89a2006db6
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 17:04:34 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,8 @@ var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
-describe('#promiseMd5()', function() {
-
-  context('with string argument', function() {
-    it('should compute MD5 hash', async function() {
-    
+describe('test()', function() {
+ it('work plis', async function() {
       await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
-    })
-    
-  })  
+    }) 
 })
