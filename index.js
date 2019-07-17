192fe1ec4aeeb73b35909cfa4fac3471af88f3f1
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 14:47:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 describe.only('#flattenArray()', function() {
-   it('Test_1',  function() {
-     driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
-     driver.findElement(webdriver.By.xpath("//div/input")).sendKeys("Selenium");
-     driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test");
-    driver.findElement(webdriver.By.xpath("//select")).click();
+    context('with string argument', function() {
+   it('Test_1', async function() {
+    await driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
+     await driver.findElement(webdriver.By.xpath("//div/input")).sendKeys("Selenium");
+     await driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test");
+    await driver.findElement(webdriver.By.xpath("//select")).click();
     {
-      const dropdown = driver.findElement(webdriver.By.id("Preferred response method"));
+      const dropdown = await driver.findElement(webdriver.By.id("Preferred response method"));
        dropdown.findElement(webdriver.By.css("*[value='Phone']")).click();
     }
-    driver.findElement(webdriver.By.xpath("//group-input/input")).sendKeys("1234566755");
-    driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select")).click();
+    await driver.findElement(webdriver.By.xpath("//group-input/input")).sendKeys("1234566755");
+    await driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select")).click();
     {
-      const dropdown = driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select"));
+      const dropdown = await driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select"));
        dropdown.findElement(webdriver.By.css("*:nth-child(2)")).click();
     }
-     driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/select")).click();
+     await driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/select")).click();
     {
-      const dropdown =  driver.findElement(webdriver.By.id("What does the feedback relate to? *"));
+      const dropdown = await driver.findElement(webdriver.By.id("What does the feedback relate to? *"));
        dropdown.findElement(webdriver.By.css("*[value='Accommodation']")).click();
     }
-    driver.findElement(webdriver.By.xpath("//textarea")).click();
-    driver.findElement(webdriver.By.xpath("//textarea")).sendKeys("Test");
-    driver.findElement(webdriver.By.css(".donate-now")).click();
-    driver.sleep(undefined);
+    await driver.findElement(webdriver.By.xpath("//textarea")).click();
+    await driver.findElement(webdriver.By.xpath("//textarea")).sendKeys("Test");
+    await driver.findElement(webdriver.By.css(".donate-now")).click();
+    await driver.sleep(undefined);
    
   });
+    })      
 })
