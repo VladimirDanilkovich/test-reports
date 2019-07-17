dfdea5052a1a86d8d7163041b8bad436253eb7d4
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 11:21:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-describe('#flattenArray()', function() {
-  // a pending test
-  it('should flatten array');
+/ Tests in this suite will run
+describe.only('#flattenArray()', function() {
+  it('should flatten array', function() {});
+})
+
+// Tests in this suite will not run
+describe('#mergeArray()', function() {
+  it('should merge two arrays', function() {});
 })
