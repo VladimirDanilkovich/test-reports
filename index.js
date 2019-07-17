979fb8f0ae9ee87a072e9b679e9ba2b7bde37645
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 13:30:05 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 describe.only('#flattenArray()', function() {
-   it('Test_1',  function() {
+   it('Test_1', acync function() {
      driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
      driver.findElement(webdriver.By.xpath("//div/input")).sendKeys("Selenium");
+     driver.findElement(webdriver.By.css(".donate-now")).click();
+     {
+      const elements = driver.findElements(webdriver.By.css(".toast-error"))
+      assert(!elements.length);
+    }
   });
 })
