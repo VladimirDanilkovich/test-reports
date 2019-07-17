8083d68f5013efc6059129a0c546f0c30071fcdd
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 13:08:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 describe.only('#flattenArray()', function() {
-   it('Test_1', async function() {
-    await driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
-   
+   it('Test_1',  function() {
+     driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
+     driver.setRect(1440, 802);  
   });
 })
