b081328b695814c072b122d3609724c1bd6afb77
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 11:40:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Tests in this suite will run
-var driver = require('selenium-webdriver');
+var webdriver = require('selenium-webdriver');
+var driver = new webdriver.Builder()
+    .forBrowser('chrome')
+    .build();
 describe.only('#flattenArray()', function() {
    it('Test_1', async function() {
     await driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
