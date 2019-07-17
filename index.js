7323dd0b76aa42deff985ab820920ea8d1eed322
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-17 16:54:23 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,32 @@ var webdriver = require('selenium-webdriver');
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
-describe.only('#flattenArray()', function() {
+describe('#promiseMd5()', function() {
+
+  context('with string argument', function() {
+    it('should compute MD5 hash', async function() {
     
-   it('Test_1', async function() {
-    await driver.get("https://sciadev-scia.cs31.force.com/feedbackform");
-     await driver.findElement(webdriver.By.xpath("//div/input")).sendKeys("Selenium");
-     await driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test");
-    await driver.findElement(webdriver.By.xpath("//select")).click();
-    {
-      const dropdown = await driver.findElement(webdriver.By.id("Preferred response method"));
-       dropdown.findElement(webdriver.By.css("*[value='Phone']")).click();
-    }
-    await driver.findElement(webdriver.By.xpath("//group-input/input")).sendKeys("1234566755");
-    await driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select")).click();
-    {
-      const dropdown = await driver.findElement(webdriver.By.xpath("//div[5]/div/form-group/div/div/div/select"));
-       dropdown.findElement(webdriver.By.css("*:nth-child(2)")).click();
-    }
-     await driver.findElement(webdriver.By.xpath("//div[2]/form-group/div/div/div/select")).click();
-    {
-      const dropdown = await driver.findElement(webdriver.By.id("What does the feedback relate to? *"));
-       dropdown.findElement(webdriver.By.css("*[value='Accommodation']")).click();
-    }
-    await driver.findElement(webdriver.By.xpath("//textarea")).click();
-    await driver.findElement(webdriver.By.xpath("//textarea")).sendKeys("Test");
-    await driver.findElement(webdriver.By.css(".donate-now")).click();
-    await driver.sleep(undefined);
-   
-  });
-     
+      // use await to wait until the promise is fulfilled
+      var hash = await promiseMd5('Glad Chinda');
+      
+      // add some assertions
+      expect(hash)
+        .to.be.a('string')
+        .that.matches(/^[a-f0-9]{32}$/)
+        .and.equal('877dbb93f50eb8a89012e15bd37ee7e4');
+    })
+    
+  })
+  context('with non-string argument', function() {
+    it('should throw an error', async function() {
+    
+      await promiseMd5(12345).catch(function(err) {
+        // add an assertion to check the error
+        expect(function() { throw err })
+          .to.throw(TypeError, 'Data must be a string or a buffer');
+       })
+       
+    })
+  })
+  
 })
