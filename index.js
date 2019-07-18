cad8103d1bea05e74e08e6e2657497be6a8fbc49
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-18 13:03:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
       
   driver = await new Builder().usingServer('http://localhost:4444/wd/hub').forBrowser('chrome').build()
     await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
-    await driver.findElement(By.xpath("//div/input")).sendKeys("Selenium")
+   /* await driver.findElement(By.xpath("//div/input")).sendKeys("Selenium")
     await driver.findElement(By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test")
     await driver.findElement(By.xpath("//select")).click()
     {
@@ -40,7 +40,7 @@
     {
       const elements = await driver.findElements(By.css(".toast-error"))
       assert(!elements.length)
-    }
+    }*/
   }
 main()
 
