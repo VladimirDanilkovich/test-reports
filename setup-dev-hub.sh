144c3a522045348090ac4945207f2a24e2740061
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-18 20:37:28 +0300

    Update setup-dev-hub.sh

--- setup-dev-hub.sh
+++ setup-dev-hub.sh
@@ -10,11 +10,8 @@
 if [ "${SFDX_AUTH_URL}" != "" ]
 then
 sudo npm install -global sfdx-cli
-
 sudo npm install -g json
-
 sudo npm install -g slack-cli
-
 FILE_NAME="sfdx-auth-url.txt"
 # Creation of a temp file that contains Sfdx Auth Url
 echo ${SFDX_AUTH_URL} > ${FILE_NAME}
@@ -24,5 +21,5 @@ sfdx force:auth:sfdxurl:store -f ${FILE_NAME} -d -a DevHub
 # Removing the file
 rm ${FILE_NAME}
 else
-    echo "It s punlic test"
+    echo "No SFDX_AUTH_URL variable. The test are run only for public available pages"
 fi
