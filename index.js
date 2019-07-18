8d524c85af34fb1c8c0c2571b72c15d395cac50e
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-18 13:17:28 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-    const { Builder,setRect, By, Key, until } = require('selenium-webdriver')
+    const { Builder, By, Key, until } = require('selenium-webdriver')
     require('selenium-webdriver/chrome')
     require('chromedriver')
     global.driver = {}
@@ -16,7 +16,6 @@
 it('Test_1', async function() {
     driver = await new Builder().usingServer('http://localhost:4444/wd/hub').forBrowser('chrome').build()
     await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
-    await driver.setRect(1440, 802)
     await driver.findElement(By.xpath("//div/input")).sendKeys("Selenium")
     await driver.findElement(By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test")
     await driver.findElement(By.xpath("//select")).click()
