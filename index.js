8b790357140fc54a3b629c9f114f6c42ceb7be60
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-18 14:41:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@
     it('Test_1', async function() {
     await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
     await driver.findElement(By.xpath("//div/input")).sendKeys("Selenium")
-    await driver.findElement(By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test")
+   // await driver.findElement(By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test")
     await driver.findElement(By.xpath("//select")).click()
     {
       const dropdown = await driver.findElement(By.id("Preferred response method"))
