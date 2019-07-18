d7872086eead693a5f37f36b3f99fd4ae1a7acea
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-18 12:41:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,10 @@
   var driver
   var vars
 
-  driver = await new Builder().usingServer('http://localhost:4444/wd/hub').forBrowser('chrome').build()
 
   it('Test_1', async function() {
+      
+  driver = await new Builder().usingServer('http://localhost:4444/wd/hub').forBrowser('chrome').build()
     await driver.get("https://sciadev-scia.cs31.force.com/feedbackform")
     await driver.findElement(By.xpath("//div/input")).sendKeys("Selenium")
     await driver.findElement(By.xpath("//div[2]/form-group/div/div/div/input")).sendKeys("Test")
