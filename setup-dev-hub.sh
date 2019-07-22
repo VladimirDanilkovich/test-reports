35fbaf652b94f9609fc733cc95678829ae460ae2
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-22 12:55:38 +0300

    Update setup-dev-hub.sh

--- setup-dev-hub.sh
+++ setup-dev-hub.sh
@@ -7,8 +7,10 @@
 #echo "Authenticating..."
 #sfdx force:auth:jwt:grant --clientid ${SFDC_PROD_CLIENTID} --jwtkeyfile keys/server.key --username ${SFDC_PROD_USER} --setdefaultdevhubusername -a DevHub
 if [ "${SFDX_AUTH_URL}" != "" ]
-then
-sudo  npm install time -g sfdx-cli  --unsafe-perm 
+thennpm  -g install express
+npm -g install express-generator
+sudo  npm install time   -g --unsafe-perm
+sudo  npm install -g sfdx-cli
 sudo npm install -g json
 sudo npm install -g slack-cli
 FILE_NAME="sfdx-auth-url.txt"
