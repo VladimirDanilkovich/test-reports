749422cab468308b86b10cbb196a6d438be663b3
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-22 11:20:49 +0300

    Update setup-dev-hub.sh

--- setup-dev-hub.sh
+++ setup-dev-hub.sh
@@ -8,7 +8,7 @@
 #sfdx force:auth:jwt:grant --clientid ${SFDC_PROD_CLIENTID} --jwtkeyfile keys/server.key --username ${SFDC_PROD_USER} --setdefaultdevhubusername -a DevHub
 if [ "${SFDX_AUTH_URL}" != "" ]
 then
-
+sudo chown -R $USER /usr/local/lib/node_modules
 npm install sfdx-cli --global
 sudo npm install -g json
 sudo npm install -g slack-cli
