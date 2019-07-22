242b50c1bb71d7ef585e0eb23c1aeab093bb1185
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-07-22 15:18:26 +0300

    Update setup-dev-hub.sh

--- setup-dev-hub.sh
+++ setup-dev-hub.sh
@@ -3,7 +3,8 @@
 #mkdir keys
 #echo ${SFDC_SERVER_KEY} | base64 -d > keys/server.key
 #
-npm install sfdx-cli --global
+#npm install sfdx-cli --global
+sudo npm install -global sfdx-cli
 ## Authenticate to salesforce
 #echo "Authenticating..."
 #sfdx force:auth:jwt:grant --clientid ${SFDC_PROD_CLIENTID} --jwtkeyfile keys/server.key --username ${SFDC_PROD_USER} --setdefaultdevhubusername -a DevHub
