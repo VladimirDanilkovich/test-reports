ae7010a621ae09080cbb2c64d3f648dff400beb7
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-01 16:51:41 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 for i in *.js;
 do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
-   echo index-updated.js
    mocha index-updated.js 
+   echo index-updated.js
 done
