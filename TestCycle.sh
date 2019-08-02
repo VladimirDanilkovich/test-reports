ef4fa845f311a44f5e8b9e55813efc3a27ab643f
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-02 12:02:33 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -2,5 +2,9 @@ for i in *.js;
 do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
    mocha index-updated.js 
-   echo index-updated.js
+   if ($(false)== 1)
+   then
+    echo "XAXAXXAXXA"
+   fi
+
 done
