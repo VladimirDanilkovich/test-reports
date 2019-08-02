4f9e4fe682e5f3be04415806ceba334b1aaf8d61
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-02 10:17:30 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+
+yum install mailutils
+
 for i in *.js;
 do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
