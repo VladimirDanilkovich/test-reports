50426369ccc0c2c096d914db90c555a1a99fa380
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-05 17:26:21 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -1,8 +1,8 @@
+var k = 0;
 for i in *.js;
 do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
-   mocha index-updated.js || echo "ERROR" 
+   mocha index-updated.js || k+1;  
    echo "${CIRCLE_NODE_INDEX}" 
-  
-
 done
+   echo"${k}"
