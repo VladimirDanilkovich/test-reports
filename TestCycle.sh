203ff9aeb4d2142795f797d910eba2632c59445e
VladimirDanilkovich <43910221+VladimirDanilkovich@users.noreply.github.com>
2019-08-05 17:50:26 +0300

    Update TestCycle.sh

--- TestCycle.sh
+++ TestCycle.sh
@@ -3,6 +3,8 @@ for i in *.js;
 do
    sed 's#Builder().forBrowser('"'"'firefox'"'"')#Builder().usingServer('"'"'http://localhost:4444/wd/hub'"'"').forBrowser('"'"'chrome'"'"')#g' "$i" > index-updated.js
    mocha index-updated.js || key=false 
-   echo "${CIRCLE_NODE_INDEX}" 
 done
-   echo"$key"
+if [ "$key" = "false" ]
+then
+echo"Cool Beans"
+fi
